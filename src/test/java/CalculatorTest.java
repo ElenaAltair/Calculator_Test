25e61673edb108082ff0c7f1b0348a2802218ea2
Elena Petrova <eop79@mail.ru>
2024-02-13 12:05:30 +0300

    changed CalculatorTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,21 +2,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+class CalculatorTest {
+
 
     @Test
-    void mainDevide() {
-        int a=2;
-        int b=0;
+    public void testDevide() {
+        int a = 2;
+        int b = 0;
         Calculator calc = Calculator.instance.get();
 
         int result = calc.devide.apply(a, b);
 
-        Assertions.assertTrue(result==0);
+        Assertions.assertTrue(result == 0);
     }
 
     @ParameterizedTest
@@ -24,8 +24,9 @@ class MainTest {
             "0,2,true",
             "0,0,true",
             "2,0,true",
+            "10,2,false"
     })
-    public void ParameterizedTest(int a, int b, boolean expected) {
+    public void testParameterized(int a, int b, boolean expected) {
         Calculator calc = Calculator.instance.get();
         int result = calc.devide.apply(a, b);
 
@@ -33,25 +34,30 @@ class MainTest {
     }
 
     @Test
-    void mainMinesPositiv() {
-        int a=10;
-        int b=-5;
+    public void testMinesPositiv() {
+        int a = 10;
+        int b = -5;
         Calculator calc = Calculator.instance.get();
 
         int result = calc.devide.apply(a, b);
 
-        Assertions.assertTrue(result<0);
+        Assertions.assertTrue(result == -2);
     }
 
-    @Test
-    void mainMinesNegetiv() {
-        int a=10;
-        int b=-5;
-
+    @ParameterizedTest
+    @CsvSource(value = {
+            "-5,-2,true",
+            "5,2,true",
+            "-5,2,true",
+            "10,-2,true",
+            "0,5,false"
+    })
+    public void testMinesPlus(int a, int b, boolean expected) {
         Calculator calc = Calculator.instance.get();
 
-        int result = calc.devide.apply(a, b);
+        boolean result = (((a > 0 && b > 0) || (a < 0 && b < 0)) && a != 0 && b != 0) ? calc.devide.apply(a, b) > 0 : calc.devide.apply(a, b) < 0;
 
-        Assertions.assertFalse(result>0);
+        Assertions.assertEquals(expected, result);
     }
+
 }
